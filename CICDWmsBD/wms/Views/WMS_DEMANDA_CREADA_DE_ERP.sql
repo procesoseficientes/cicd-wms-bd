a1d730ed61035647d6dab56fe4ac9656dc99194d
Gustavowgc <64803356+Gustavowgc@users.noreply.github.com>
2022-08-26 23:14:50 -0600

    error por no tener todas las comlumnas

--- CICDWmsBD/wms/Views/WMS_DEMANDA_CREADA_DE_ERP.sql
+++ CICDWmsBD/wms/Views/WMS_DEMANDA_CREADA_DE_ERP.sql
@@ -1,4 +1,4 @@
-﻿CREATE VIEW [wms].[WMS_DEMANDA_CREADA_DE_ERP] (
+CREATE VIEW [wms].[WMS_DEMANDA_CREADA_DE_ERP] (
     [DocNum],
     [DocEntry],
     [OlaDePicking],
@@ -10,6 +10,13 @@
 )
 WITH ENCRYPTION
 AS
-SELECT NULL AS [NullColumn]
---The script body was encrypted and cannot be reproduced here.;
+SELECT NULL AS [DocNum],
 
+    NULL AS [DocEntry],
+    NULL AS [OlaDePicking],
+    NULL AS [FacturaDocNum],
+    NULL AS [FacturaDocEntry],
+    NULL AS [OWNER],
+    NULL AS [RespuestaDesdeERP],
+    NULL AS [NumeroManifiesto]
+--The script body was encrypted and cannot be reproduced here.;
