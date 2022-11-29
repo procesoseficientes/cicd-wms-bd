1001b931c52a58317cd1ce205381cd21cf57a924
elder.l <elucas-2015337@kinal.edu.gt>
2022-11-29 16:48:47 -0600

    Se agrega validación de versión

--- CICDWmsBD/wms/Stored Procedures/OP_WMS_SP_VALIDATE_CREDENTIALS_OF_USER.sql
+++ CICDWmsBD/wms/Stored Procedures/OP_WMS_SP_VALIDATE_CREDENTIALS_OF_USER.sql
@@ -10,14 +10,16 @@
 -- Ejemplo de Ejecucion:
 				EXEC [wms].[OP_WMS_SP_VALIDATE_CREDENTIALS_OF_USER]
 				@LOGIN_ID = 'ADMIN'
-				,@DOMAIN = 'wms'
-				,@PASSWORD = '124'
+				,@DOMAIN = 'ALZAQA'
+				,@PASSWORD = '@wms**'
+				,@VERSION_BO='2022.10.0349'
 */
 -- =============================================
 CREATE PROCEDURE [wms].[OP_WMS_SP_VALIDATE_CREDENTIALS_OF_USER] (
 		@LOGIN_ID VARCHAR(50)
 		,@DOMAIN VARCHAR(50)
 		,@PASSWORD VARCHAR(50)
+		,@VERSION_BO VARCHAR(12)
 	)
 AS
 BEGIN
@@ -25,8 +27,13 @@ BEGIN
 	SET NOCOUNT ON;
 	
 	--
-	DECLARE	@ERROR VARCHAR(MAX);
-
+	DECLARE	@ERROR VARCHAR(MAX)
+	
+	DECLARE @VERSION_QA VARCHAR(12);
+	DECLARE @VERSION_PROD VARCHAR(12);
+	SELECT @VERSION_QA = VALUE FROM [wms].[OP_WMS_PARAMETER] WHERE [IDENTITY] = 50
+	SELECT @VERSION_PROD = VALUE FROM [wms].[OP_WMS_PARAMETER] WHERE [IDENTITY] = 47
+	PRINT @VERSION_QA + @VERSION_PROD
 	-- ------------------------------------------------------------------------------------
 	-- valido si esta activo el usuario
 	-- ------------------------------------------------------------------------------------
@@ -75,10 +82,18 @@ BEGIN
 					[L].[LOGIN_ID] = @LOGIN_ID
 					AND [L].[LOGIN_PWD] = @PASSWORD) = 1
 			BEGIN
+			IF(@VERSION_QA = @VERSION_BO OR @VERSION_PROD = @VERSION_BO) 
+				BEGIN
 				SELECT
 					1 AS [Resultado]
 					,'Proceso Exitoso' [Mensaje]
 					,1 [Codigo];
+			END;
+			ELSE
+			BEGIN
+			SET @ERROR = 'ACTUALIZAR A LA ÚLTIMA VERSIÓN'
+				RAISERROR(@ERROR,16,1);
+			END;
 			END;
 			ELSE
 			BEGIN
