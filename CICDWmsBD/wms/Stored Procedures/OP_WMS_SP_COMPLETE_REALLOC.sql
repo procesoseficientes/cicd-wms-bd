81ec833897504c9699bb65776b3362ba0c52b575
elder.l <elucas-2015337@kinal.edu.gt>
2023-01-30 16:42:09 -0600

    SP COMPLETE REALLOC

---
+++ CICDWmsBD/wms/Stored Procedures/OP_WMS_SP_COMPLETE_REALLOC.sql
@@ -0,0 +1,15 @@
+﻿create PROCEDURE [wms].[OP_WMS_SP_COMPLETE_REALLOC]
+	@TASK_ID int
+AS
+	UPDATE [wms].[OP_WMS_TASK] SET 
+	COMPLETED_DATE = GETDATE(),
+	IS_COMPLETE = 1,
+	LAST_UPDATE = GETDATE()
+	WHERE TASK_ID = @TASK_ID
+
+
+	SELECT
+		1 AS [Resultado]
+		,'Proceso Exitoso' [Mensaje]
+		,0 [Codigo]
+		,'' [DbData];
